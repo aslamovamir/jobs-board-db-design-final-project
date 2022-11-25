94942cb8978b50398da0f7aca58ed0763507586c
aslamovamir <90361791+aslamovamir@users.noreply.github.com>
2022-11-24 21:06:36 -0500

    initial SQL database set up
    1. added a folder for database related actions
    2. created a class for initial database set up
    3. created 4 methods to set up database creation for the Applicant and Company

---
+++ source/database/SQLiteDBSetUp.py
@@ -0,0 +1,32 @@
+import sqlite3
+from dataclasses import dataclass
+from model.Applicant.ApplicantUser import ApplicantUser
+from model.Applicant.ApplicantProfile import ApplicantProfile
+from model.Company.CompanyUser import CompanyUser
+from model.Company.CompanyProfile import CompanyProfile
+
+
+#NOTE: Methods of this class must be run only once: they are run when running the app for
+# the first time internally by developers to set up the table and the neceassary tables 
+# in the sql database
+
+# class for the database set-up
+@dataclass
+class DatabaseSetUp:
+
+    # method to create an SQL table for the ApplicantUser entity
+    def CreateApplicantUserTable(applicantUser: ApplicantUser) -> bool:
+        pass
+
+    # method to create an SQL table for the ApplicantProfile entity
+    def CreateApplicantProfileTable(applicantProfile: ApplicantProfile) -> bool:
+        pass
+
+    # method to create an SQL table for the CompanyUser entity
+    def CreateCompanyUserTable(companyUser: CompanyUser) -> bool:
+        pass
+
+    # method to create an SQL table for the CompanyProfile entity
+    def CreateCompanyProfileTable(companyProfile: CompanyProfile) -> bool:
+        pass
+
