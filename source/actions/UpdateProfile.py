34de316c199d3c74dfb781007fa92b0b19cb8e90
aslamovamir <90361791+aslamovamir@users.noreply.github.com>
2022-11-29 13:29:25 -0500

    Update UpdateProfile.py

--- source/actions/UpdateProfile.py
+++ source/actions/UpdateProfile.py
@@ -268,95 +268,26 @@ class UpdateProfile:
 
                 # take in the menu option entered
                 decision: int = MenuHelper.InputOption()
-
-                #TODO: CHANGE THE INPUT OPTIONS TO THE COMPANY PROFILE PARAMETERS, FINISH!
-                # update title
-                if decision == 1:
-                    while True:
-                        try:
-                            print("\nPlease enter a new title")
-                            input = MenuHelper.InputStream()
-                            if input == "-1":
-                                break
-                            newProfile.Title = input
-                            if MenuHelper.ValidateEmptyInput(input=newProfile.Title):
-                                MenuHelper.WarnInvalidInput()
-                                continue
-                            print("Success! Your title changed to: ", newProfile.Title)
-                            break
-                        except:
-                            MenuHelper.WarnInvalidInput()
-
+                
                 # update about
-                elif decision == 2:
+                if decision == 1:
                     while True:
                         try:
                             print("\nPlease enter a new about section")
                             input = MenuHelper.InputStream()
                             if input == "-1":
                                 break
-                            newProfile.About = input
+                            newProfile.About= input
                             if MenuHelper.ValidateEmptyInput(input=newProfile.About):
                                 MenuHelper.WarnInvalidInput()
                                 continue
-                            print("Success! Your about section changed to: ", newProfile.About)
+                            print("Success! The company title changed to: ", newProfile.About)
                             break
                         except:
                             MenuHelper.WarnInvalidInput()
-                
-                # update gender
-                elif decision == 3:
-                    while True:
-                        try:
-                            print("\nPlease enter a new gender")
-                            input = MenuHelper.InputStream()
-                            if input == "-1":
-                                break
-                            newProfile.Gender = input
-                            if MenuHelper.ValidateEmptyInput(input=newProfile.Gender):
-                                MenuHelper.WarnInvalidInput()
-                                continue
-                            print("Success! Your gender changed to: ", newProfile.Gender)
-                            break
-                        except:
-                            MenuHelper.WarnInvalidInput()
-                
-                # update ethnicity
-                elif decision == 4:
-                    while True:
-                        try:
-                            print("\nPlease enter a new ethnicity")
-                            input = MenuHelper.InputStream()
-                            if input == "-1":
-                                break
-                            newProfile.Ethnicity = input
-                            if MenuHelper.ValidateEmptyInput(input=newProfile.Ethnicity):
-                                MenuHelper.WarnInvalidInput()
-                                continue
-                            print("Success! Your ethnicity changed to: ", newProfile.Ethnicity)
-                            break
-                        except:
-                            MenuHelper.WarnInvalidInput()
-                
-                # update disability status
-                elif decision == 5:
-                    while True:
-                        try:
-                            print("\nPlease enter a new disability status")
-                            input = MenuHelper.InputStream()
-                            if input == "-1":
-                                break
-                            newProfile.DisabilityStatus = input
-                            if MenuHelper.ValidateEmptyInput(input=newProfile.DisabilityStatus):
-                                MenuHelper.WarnInvalidInput()
-                                continue
-                            print("Success! Your disability status changed to: ", newProfile.DisabilityStatus)
-                            break
-                        except:
-                            MenuHelper.WarnInvalidInput()
-                    
+
                 # update location
-                elif decision == 6:
+                elif decision == 2:
                     while True:
                         try:
                             print("\nPlease enter a new location")
@@ -367,24 +298,126 @@ class UpdateProfile:
                             if MenuHelper.ValidateEmptyInput(input=newProfile.Location):
                                 MenuHelper.WarnInvalidInput()
                                 continue
-                            print("Success! Your location changed to: ", newProfile.Location)
+                            print("Success! The company location changed to: ", newProfile.Location)
                             break
                         except:
                             MenuHelper.WarnInvalidInput()
                 
-                # update phone number
-                elif decision == 7:
+                # update industry
+                elif decision == 3:
                     while True:
                         try:
-                            print("\nPlease enter a new phone number")
+                            print("\nPlease enter a new industry")
                             input = MenuHelper.InputStream()
                             if input == "-1":
                                 break
-                            newProfile.PhoneNumber = input
-                            if MenuHelper.ValidateEmptyInput(input=newProfile.PhoneNumber):
+                            newProfile.Industry = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.Industry):
                                 MenuHelper.WarnInvalidInput()
                                 continue
-                            print("Success! Your phone number changed to: ", newProfile.PhoneNumber)
+                            print("Success! The company industry changed to: ", newProfile.Industry)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                
+                # update service type
+                elif decision == 4:
+                    while True:
+                        try:
+                            print("\nPlease enter a new service type")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.ServiceType = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.ServiceType):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company service type changed to: ", newProfile.ServiceType)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                
+                # update company type
+                elif decision == 5:
+                    while True:
+                        try:
+                            print("\nPlease enter a new company type")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.CompanyType = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.CompanyType):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company type changed to: ", newProfile.CompanyType)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                    
+                # update annual revenue
+                elif decision == 6:
+                    while True:
+                        try:
+                            print("\nPlease enter a new annual revenue")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.AnnualRevenue = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.AnnualRevenue):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company annual revenue changed to: ", newProfile.AnnualRevenue)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                
+                # update employee size
+                elif decision == 7:
+                    while True:
+                        try:
+                            print("\nPlease enter a new employee size")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.EmployeeSize = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.EmployeeSize):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company employee size changed to: ", newProfile.EmployeeSize)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                
+                # update foundation date
+                elif decision == 8:
+                    while True:
+                        try:
+                            print("\nPlease enter a new foundation date")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.FoundationDate = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.FoundationDate):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company foundation date changed to: ", newProfile.FoundationDate)
+                            break
+                        except:
+                            MenuHelper.WarnInvalidInput()
+                
+                # update international hires
+                elif decision == 9:
+                    while True:
+                        try:
+                            print("\nPlease enter a new international hires section")
+                            input = MenuHelper.InputStream()
+                            if input == "-1":
+                                break
+                            newProfile.InternationalHires = input
+                            if MenuHelper.ValidateEmptyInput(input=newProfile.InternationalHires):
+                                MenuHelper.WarnInvalidInput()
+                                continue
+                            print("Success! The company international hires section changed to: ", newProfile.InternationalHires)
                             break
                         except:
                             MenuHelper.WarnInvalidInput()
@@ -397,14 +430,8 @@ class UpdateProfile:
         
         while True:
             try:
-                print("\nThis is your new profile information: ")
-                print("Title: ", newProfile.Title)
-                print("About: ", newProfile.About)
-                print("Gender: ", newProfile.Gender)
-                print("Ethnicity: ", newProfile.Ethnicity)
-                print("Disability Status: ", newProfile.DisabilityStatus)
-                print("Location: ", newProfile.Location)
-                print("Phone Number: ", newProfile.PhoneNumber)
+                print("\nThis is the company's new profile information: ")
+                # TODO: FINISH!
 
                 # ask for change confirmation
                 if MenuHelper.ConfirmChanges():
